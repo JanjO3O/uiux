29dbb61bec0f303050fd349ddd41d05bd3c361df
JanjO3O <jinjoo7438@gmail.com>
2026-02-10 13:11:44 +0900

    260210_오전수업

--- sdream/js/main.js
+++ sdream/js/main.js
@@ -55,4 +55,25 @@ $(document).ready(function(){
     })
 
 
+    /******************************************************
+     * 웹진 swiper
+     *****************************************************/ 
+
+    const webzin_swiper = new Swiper('.webzin .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,  /* 여백 */ 
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: false,  /* 뉴스는 돌면 안되고, 마지막에서 멈춰야 함.(마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기) */
+        navigation: {
+            nextEl: '.webzin .ctrl_wrap .next',
+            prevEl: '.webzin .ctrl_wrap .prev',
+        },
+    });//webzin_swiper
+
 })//$(document)
