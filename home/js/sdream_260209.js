9a4deeea2b837315367601491b37625f1c22d3cb
JanjO3O <jinjoo7438@gmail.com>
2026-02-13 10:44:05 +0900

    2660212_home.

--- home/js/sdream_260209.js
+++ home/js/sdream_260209.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    // const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-    //     autoplay: {  /* 팝업 자동 실행 */
+    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        autoplay: {  /* 팝업 자동 실행 */
             
-    //         delay: 5000,  //--> 5s
-    //         disableOnInteraction: true,
-    //     },
+            delay: 5000,  //--> 5s
+            disableOnInteraction: true,
+        },
 
-    //     effect: "fade", /* fade 효과 */
-    //     loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    // });//visual_swiper
+        effect: "fade", /* fade 효과 */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    });//visual_swiper
 
 
 
@@ -45,5 +45,32 @@ $(document).ready(function(){
     header_fixed()
     $(window).scroll(function(){
         header_fixed()
-    })
+    })//.header
+
+
+
+    // 웹진 START
+    const webzine_swiper = new Swiper('.webzine .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 12, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            768: {    /* 768px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 해제 */
+        autoplay: {  /* 팝업 자동 실행 */
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        navigation: {
+            nextEl: '.webzine .ctrl_wrap .next',
+            prevEl: '.webzine .ctrl_wrap .prev',
+        },
+    });
+
+
+
+
 })//$(document).ready
