2b70b84f203f158f95fcd4289fd4569fa27c09f9
JanjO3O <jinjoo7438@gmail.com>
2026-02-17 02:02:01 +0900

    260216_task

--- Task/js/KCC.main.js
+++ Task/js/KCC.main.js
@@ -1,3 +1,8 @@
 $(document).ready(function(){
-    console.log('나오니??')
-});
+
+    $('.recruit .list ul li').on('mouseenter', function(){
+        console.log('오버함!!')
+        $('.recruit .list ul li').removeClass('active')
+        $(this).addClass('active')
+    })
+});//$(document)
