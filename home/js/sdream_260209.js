e15e31f68a5408873433ac43b340c21e56ebba17
JanjO3O <jinjoo7438@gmail.com>
2026-02-16 22:42:56 +0900

    260216_home 전체반응형

--- home/js/sdream_260209.js
+++ home/js/sdream_260209.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-    const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
-        autoplay: {  /* 팝업 자동 실행 */
+    // const visual_swiper = new Swiper('.visual .swiper', { /* 팝업을 감싼는 요소의 class명 */
+    //     autoplay: {  /* 팝업 자동 실행 */
             
-            delay: 5000,  //--> 5s
-            disableOnInteraction: true,
-        },
+    //         delay: 5000,  //--> 5s
+    //         disableOnInteraction: true,
+    //     },
 
-        effect: "fade", /* fade 효과 */
-        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
-    });//visual_swiper
+    //     effect: "fade", /* fade 효과 */
+    //     loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+    // });//visual_swiper
 
 
 
@@ -28,7 +28,7 @@ $(document).ready(function(){
 
     function header_fixed(){  // 함수의 정의
         scrolling = $(window).scrollTop()
-        console.log(scrolling, prev_scroll, prev_scroll-scrolling)
+        // console.log(scrolling, prev_scroll, prev_scroll-scrolling)
         if(scrolling > 0){
             $('.header').addClass('fixed')
             move_scroll = prev_scroll - scrolling
@@ -96,6 +96,15 @@ $(document).ready(function(){
             prevEl: '.webzin .ctrl_wrap .prev',
         },
     });
+
+    // top 버튼
+    $('.footer .top').on('click', function(){
+        console.log('클릭')
+        // $(window).scrollTop(0)
+        $('html,body').animate({
+            scrollTop : 0
+        }, 500)
+    })//.footer .top
 
 
 
