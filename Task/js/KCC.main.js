52006904a80278a9be861f4d7a8e24f84d9a0c8a
JanjO3O <jinjoo7438@gmail.com>
2026-02-18 16:58:32 +0900

    js

--- Task/js/KCC.main.js
+++ Task/js/KCC.main.js
@@ -5,4 +5,37 @@ $(document).ready(function(){
         $('.recruit .list ul li').removeClass('active')
         $(this).addClass('active')
     })
+
+
+
+    
+    // function recruitActive(){
+
+    //     let winWidth = $(window).width(window > 1025);
+
+    //     // 기존 이벤트 제거 (중복 방지)
+    //     $(".recruit .list ul li").off("click");
+
+    //     if(winWidth > 1025){
+
+    //         $(".recruit .list ul li").on("click", function(){
+
+    //             $(".recruit .list ul li").removeClass("active");
+    //             $(this).addClass("active");
+
+    //         });
+
+    //     }
+
+    // }
+
+    // // 처음 실행
+    // recruitActive();
+
+    // // 리사이즈 시 다시 체크
+    // $(window).on("resize", function(){
+    //     recruitActive();
+    // });
+
+
 });//$(document)
