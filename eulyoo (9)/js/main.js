6b591d20aebfd24137e6a37a517df56094fc8210
JanjO3O <jinjoo7438@gmail.com>
2026-02-26 17:36:15 +0900

    260226_완료

--- eulyoo (9)/js/main.js
+++ eulyoo (9)/js/main.js
@@ -190,5 +190,34 @@ $(document).ready(function(){
         },
     });
 
+    
+    
+    /***********************************
+     * story
+     **********************************/ 
+
+    const story_swiper = new Swiper('.story .swiper', { /* 팝업을 감싼는 요소의 class명 */
+        slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            // 451: {    /* 451px 이상일때 적용 */
+            //     slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+            //     spaceBetween: 24,
+            // },
+            769: {    /* 769px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+            1025: {    /* 1025px 이상일때 적용 */
+                slidesPerView: 4,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 24,
+            },
+        },
+        navigation: {
+            nextEl: '.story .next',
+            prevEl: '.story .prev',
+        },
+    });
+
 
 }) //$(document).ready
