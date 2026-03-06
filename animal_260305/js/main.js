c9822271f6c643dae79aa52c64ddfa9c18af5767
JanjO3O <jinjoo7438@gmail.com>
2026-03-06 17:58:50 +0900

    260306_학원수업

--- animal_260305/js/main.js
+++ animal_260305/js/main.js
@@ -129,11 +129,11 @@ $(document).ready(function(){
 
     
     /*******************************************************
-     * message는 높이가 높고 그 안에 inner가 고정으로 있음 sticky
+     * message는 높이가 높고 그 안에 inner가 고정으로 있음 --> sticky
      * message가 스크롤되는 동안 p strong span 태그가 넓이가 넓어지면서 색상 바뀜
     *******************************************************/
 
-    let color_obj = $('.message p strong')
+    let color_obj = $('.message p strong')  // 얘는 strong, span 두개 다 포함
     let color_area = $('.message')
     let color_resizer = 'span'
     let color_line = color_obj.length //몇줄인지
@@ -153,16 +153,16 @@ $(document).ready(function(){
     // color_obj.eq(2).find(color_resizer).width('50%') //eq는 카운트를 0부터 시작함
 
     function color_change(){
-        scrolling = $(window).scrollTop()
+        scrolling = $(window).scrollTop()  //브라우저가 스크롤 되는 값
         color_win_h = $(window).height()
         color_header = $('.header').height()
         
-        color_start = color_area.offset().top
-        color_end = color_area.offset().top + color_area.height()
+        // color_start = color_area.offset().top  //--> header와 상관없이 .message가 브라우저의 상단의 딱 맞닿는 지점에서부터 색 변경 시작됨
+        // color_end = color_area.offset().top + color_area.height()
 
 
-        // color_start = color_area.offset().top - color_win_h/2
-        // color_end = color_area.offset().top + color_area.height() - color_win_h/2
+        color_start = color_area.offset().top - color_win_h/2  // 색 변경 시작 = .message한테 document의 top의 0 값에서부터(=즉, 메세지가 브라우저의 상단의 딱 맞닿는 지점에서) - 브라우저의 높이의 반띵 한 지점에서부터 색 변경 시작
+        color_end = color_area.offset().top + color_area.height() - color_win_h/2
 
 
         if(color_end < scrolling){
@@ -208,6 +208,93 @@ $(document).ready(function(){
     color_change()  //문서가 로딩되고 단 1번 실행
     $(window).scroll(function(){ 
         color_change()  //스크롤될 때마다 실행
+    })
+
+
+    /********************************************
+     * .adopt slick
+     *********************************************/ 
+
+    $('.adopt .list_wrap .list').slick({ // 넓이를 자유롭게 설정해서 css에서 list_item 입력한 width: clamp(240px, 36vw, 342px); 값이 적용됨
+                                        
+        autoplay: false, //팝업 자동 실행
+        autoplaySpeed: 3000, //팝업이 머무는 시간
+        speed: 500, //팝업 전환 속도
+        dots: false, //하단 페이지 버튼 (true, false)
+        arrows: false,  //다음, 이전팝업 (true, false)
+        //pauseOnHover: true, //마우스호버시 일시정지
+        infinite: true, //무한반복
+        variableWidth: true, //넓이를 자유롭게 설정  --> 반응형이 적용 안됨 --> css에서 쓴 넓이가 적용됨 
+        slidesToShow: 6, //한번에 보일 팝업 수
+        //slidesToScroll: 1, //한번 드래그에 움직이는 슬라이드 제한
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        centerMode: true, //가운데정렬(가운데가 1번)
+         responsive: [
+            {
+                breakpoint: 769, //768px 이하
+                settings: {
+                    centerMode: false,  //  모바일 사이즈에서는 --> 가운데 정렬하는거 취소함.
+                }
+            },
+        ]
+    });
+
+
+
+    //review slick
+    $('.review .list_wrap .list').slick({  // --> js에서 팝업 수를 제한해서 css에서 입력한 width값 무시됨
+        autoplay: false, //팝업 자동 실행
+        autoplaySpeed: 3000, //팝업이 머무는 시간
+        speed: 500, //팝업 전환 속도
+        dots: false, //하단 페이지 버튼 (true, false)
+        arrows: false,  //다음, 이전팝업 (true, false)
+        //pauseOnHover: true, //마우스호버시 일시정지
+        //infinite: false, //무한반복
+        //variableWidth: true, //넓이를 자유롭게 설정
+        slidesToShow: 4, //한번에 보일 팝업 수 
+        //slidesToScroll: 1, //한번 드래그에 움직이는 슬라이드 제한
+        swipeToSlide: true, //드래그한만큼 슬라이드 움직이기
+        //centerMode: true, //가운데정렬(가운데가 1번)
+        responsive: [
+            {
+                breakpoint: 1025, //1024 이하
+                settings: {
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 769, //768 이하
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 376, //375 이하
+                settings: {
+                    slidesToShow: 1
+                }
+            },
+        ]
+    });
+
+    // .footer .top
+    $('.footer .top').on('click', function(){
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500)
+    })
+
+    let footer_h
+
+    function footer_count(){  //함수의 선언
+        footer_h = $('.footer').outerHeight()
+        console.log(footer_h)
+        $('.container').css('margin-bottom', footer_h)
+    }
+
+    footer_count()  //함수의 실행  -- 브라우저가 로딩되고 단 한 번 실행
+    $(window).resize(function(){
+        footer_count()  //함수의 실행  -- 브라우저가 리사이즈될 때마다 실행
     })
 
 })//$(document).ready
