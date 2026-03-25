1421b7a06c2c46d655c1c317c13e34921d2ac9ae
JanjO3O <jinjoo7438@gmail.com>
2026-03-25 23:55:08 +0900

    260325_금호

--- Portfolio/kumho/js/main.js
+++ Portfolio/kumho/js/main.js
@@ -157,14 +157,14 @@ $(document).ready(function(){
 
     // 2. 스와이퍼 선언 부분
     const collect01_swiper = new Swiper('.collection #collect_conts01 .swiper', { 
-        slidesPerView: 2, 
-        spaceBetween: 12, 
+        slidesPerView: 'auto', //425 이하부터
+        spaceBetween: 6, 
         breakpoints: {
-            768: { 
+            426: { //1024~426
                 slidesPerView: 'auto',
                  spaceBetween: 12 
             },
-            1024: { 
+            1025: { //1025 이상
                 slidesPerView: 'auto',
                  spaceBetween: 24 
             },
@@ -192,14 +192,14 @@ $(document).ready(function(){
     });
 
     const collect02_swiper = new Swiper('.collection #collect_conts02 .swiper', { 
-        slidesPerView: 2, 
-        spaceBetween: 12, 
+        slidesPerView: 'auto', //425 이하부터
+        spaceBetween: 6, 
         breakpoints: {
-            768: { 
-                slidesPerView: 3,
+            426: { //1024~426
+                slidesPerView: 'auto',
                  spaceBetween: 12 
             },
-            1024: { 
+            1025: { //1025 이상
                 slidesPerView: 'auto',
                  spaceBetween: 24 
             },
@@ -256,6 +256,23 @@ $(document).ready(function(){
     });
 
     
+
+    const business_swiper = new Swiper('.business .swiper', { /* 팝업을 감싸는 요소의 class명 */
+        slidesPerView: 2, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            640: {    /* 640px 이상일때 적용 */
+                slidesPerView: 3,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 20,
+            },
+        },
+        //centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
+        loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+    });
 
 
 
