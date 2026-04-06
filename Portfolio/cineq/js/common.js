0540db8ed95b325f416d0fd009ea37222bc1963c
JanjO3O <jinjoo7438@gmail.com>
2026-04-06 18:04:37 +0900

    260406_포폴2

---
+++ Portfolio/cineq/js/common.js
@@ -0,0 +1,40 @@
+$(document).ready(function(){
+    
+    let lang_wrap = $('.header_wrap .header_banner .lang_wrap')
+    let lang_list = $('.header_wrap .header_banner .lang_wrap .lang_list')
+    let header_action = $('.header_wrap .header_banner .header_action')
+
+    $(lang_wrap).on('click focusin', function(e){
+        e.stopPropagation()
+        $(this).addClass('open')
+        $(this).find(lang_list).show()
+    })
+    $(lang_list).find('li > a').on('click', function(e){
+        e.stopPropagation()
+        lang_wrap.removeClass('open')
+        lang_list.hide()
+    })
+    $(header_action).on('focusin', function(){
+        lang_wrap.closest('open')
+        lang_list.hide()
+    })
+
+
+
+    
+
+    $('.header_wrap .header_bot .gnb ul.depth1 > li').on('mouseenter ', function(){
+        $('.header_wrap .header_bot .gnb ul.depth1 > li').removeClass('over')
+        $(this).addClass('over')
+        $('.header_wrap .header_bot .gnb ul.depth1 > li > ul.depth2').slideUp()
+        $(this).find('ul.depth2').slideDown()
+        $('.header_wrap .header_bot .gnb .depth2_bg').slideDown()
+    })
+    $('.header_wrap .header_bot .gnb').on('mouseleave ', function(){
+        $('.header_wrap .header_bot .gnb ul.depth1 > li').removeClass('over')
+        $('.header_wrap .header_bot .gnb ul.depth1 > li > ul.depth2').slideUp()
+        $('.header_wrap .header_bot .gnb .depth2_bg').slideUp()
+    })
+    
+    
+})
