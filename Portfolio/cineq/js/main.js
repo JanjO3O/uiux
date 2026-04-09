e267652f4455342baf3fed84dff4ccd5fd2413a6
JanjO3O <jinjoo7438@gmail.com>
2026-04-09 18:00:28 +0900

    260409_포폴2

--- Portfolio/cineq/js/main.js
+++ Portfolio/cineq/js/main.js
@@ -47,16 +47,16 @@ $(document).ready(function(){
     
     const Box_Office_swiper = new Swiper('.Box_Office .swiper', {
         slidesPerView: 'auto', /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
-        spaceBetween: 16, /* 팝업과 팝업 사이 여백 */
-        speed: 2000,
+        spaceBetween: 5, /* 팝업과 팝업 사이 여백 */
+        // speed: 5000,
         // autoplay: {  /* 팝업 자동 실행 */
         //     delay: 0,
-        //     disableOnInteraction: false,
         // },
+        disableOnInteraction: false,
         breakpoints: {
             769: {    /* 769px 이상일때 적용 */
                 slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
-                spaceBetween: 24,  //coverflow 효과로 간격조절 할거라서 0 준거임
+                spaceBetween: 5,  //coverflow 효과로 간격조절 할거라서 0 준거임
             },
         },
         centeredSlides: true, /* 팝업을 화면에 가운데 정렬(가운데 1번이 옴) */
@@ -65,58 +65,127 @@ $(document).ready(function(){
         grabCursor: true,
         coverflowEffect: {
             rotate: 10,       // 좌우 회전 각도
-            stretch: 0,     // 슬라이드 간격 (겹침 느낌)
-            depth: 0,       // Z축 깊이
-            modifier: 1,      // 강도
-            scale: 1.005,
+            stretch: 5,     // 슬라이드 간격 (겹침 느낌)
+            depth: -70,       // Z축 깊이
+            modifier: 1.5,      // 강도
+            scale: 1,
             slideShadows: false,   //slide 그림자 적용 여부
         },
 
         watchSlidesProgress: true,
 
-        on: {
-            // progress: function () {
-            //     this.slides.forEach((slide) => {
-            //         let progress = slide.progress;
-
-            //         // 여기서 커스텀 시작
-            //         let angle = progress * Math.PI / 5000000; // 각도 범위 조절 --> 값 크면 더 크게 휘어짐 / 작으면 완만한 곡선
-            //         let radius = 400; // 원 반지름 --> 크면 완만한 곡선 / 작으면 급격한 곡선
-
-            //         // 원형 좌표 계산
-            //         let x = radius * Math.sin(angle);
-            //         let y = radius * (1 - Math.cos(angle)); // 아래쪽 기준으로 보정
-
-
-            //         // let x = progress * 280; // 좌우 이동
-            //         // let y = -Math.pow(progress, 2) * 120; // 위로 올라가는 곡선
-            //         let scale = 1 - Math.pow(Math.abs(progress), 1.2) * 4;  //끝으로 갈수록 더 자연스럽게 줄어듦
-            //         let rotateY = progress * 0;
-            //         // let rotate = progress * 50;
-            //         let zIndex = 100 - Math.abs(progress) * 10;
-            //         let opacity = 1 - Math.abs(progress) * 0; /* 마지막 숫자 값이 0.5면 좌우에 0.5씩 이미지가 투명해짐 */
-            //         let blur = Math.abs(progress) * 0.5;
-
-            //         slide.style.transform = `
-            //             translateX(${x}px)
-            //             translateY(${y}px)
-            //             scale(${scale})
-            //             rotateY(${rotateY}deg)
-            //         `;
-            //         slide.style.zIndex = zIndex;
-            //         slide.style.opacity = opacity;
-            //         slide.style.filter = `blur(${blur}px)`;
-            //     });
-            // },
-            // setTransition: function (duration) {
-            //     this.slides.forEach((slide) => {
-            //         slide.style.transition = duration + 'ms';
-            //     });
-            // },
-        },
+        
     
 	loop: true,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
 	
-});
+    });
+
+
+
+
+
+
+    let tab_list = $('.biz .tab_list ul li')
+    let tab_name
+
+    tab_list.on('click', function(){
+        tab_list.removeClass('active')
+        tab_list.attr('aria-selected', 'false')
+        tab_list.find('button em').text('')
+        $(this).addClass('active')
+        $(this).attr('aria-selected', 'true')
+        $(this).find('button em').text('선택됨')
+        tab_name = $(this).attr('aria-controls')
+
+        $('.biz .tab_conts .tab_item').removeClass('active')
+        $('.biz .tab_conts').find('#' + tab_name).addClass('active')
+    })
+
+
+    let line_area = $('.biz')  //해당 영역
+    let line1 = $('.biz .line1')
+    let line2 = $('.biz .line2')
+
+    let win_h //브라우저 높이
+    let head_h //header 높이
+    
+    let line1_start  //line1 시작 위치
+    let line1_end  //line1 종료 위치
+    let line2_start  //line2 시작 위치
+    let line2_end  //line2 종료 위치
+    
+    let scrolling //현재 스크롤 된 값
+    let line_diff  //시작 이후에 얼마나 스크롤 되었는지
+
+
+    function change(){
+        scrolling = $(window).scrollTop() //브라우저 스크롤 값
+        win_h = $(window).height()
+        head_h = $('.header_wrap').height()
+
+        line1_start = line_area.offset().top
+        line1_end = line_area.offset().top + win_h/4
+
+        line2_start = line_area.offset().top + win_h/4 //line1 종료지점에서부터 시작
+        line2_end = line_area.offset().top + win_h/2  //반띵한 높이에서  종료
+
+        if(line1_end < scrolling){
+            line1.css('transform', 'translateX(0)')
+            // console.log('끝')
+        }else if(line1_start > scrolling){
+            // console.log('시작 전')
+            line1.css('transform', 'translateX(-100%)')
+        }else{
+            // console.log('진행중')
+            line_diff = scrolling - line1_start
+        }
+
+        if(line2_end < scrolling){
+            line2.css('transform', 'translateX(0)')
+            // console.log('끝')
+        }else if(line2_start > scrolling){
+            // console.log('시작 전')
+            line2.css('transform', 'translateX(100%)')
+        }else{
+            // console.log('진행중')
+            line_diff = scrolling - line2_start
+        }   
+    }
+
+    change()  //문서가 로딩되고 단 한 번 실행
+    $(window).scroll(function(){ 
+        change() //스크롤 될 때마다
+    })
+
+
+    const biz_swiper = new Swiper('.biz .swiper', {
+        slidesPerView: 1, /* 한번에 보일 팝업의 수 - 모바일 제일 작은 사이즈일때 */
+        spaceBetween: 12, /* 팝업과 팝업 사이 여백 */
+        breakpoints: {
+            501: {    /* 501px 이상일때 적용 */
+                slidesPerView: 2,    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 12,
+            },
+            651: {    /* 651px 이상일때 적용 */
+                slidesPerView: 'auto',    /*    'auto'   라고 쓰면 css에서 적용한 넓이값이 적용됨 */
+                spaceBetween: 12,
+            },
+        },
+        loop: false,  /* 마지막 팝업에서 첫번째 팝업으로 자연스럽게 넘기기 */
+        // autoplay: {  /* 팝업 자동 실행 */
+        //     delay: 2500,
+        //     disableOnInteraction: true,
+        // },
+    });
+
+
+
+    AOS.init({
+        offset: 150, // 해당 콘텐츠가 하단에서 몇 px 위로 올라와에 나타나는 효과가 나타날지 셋팅하는 값
+        duration: 500, // 애니메이션 효과가 작동되는 시간
+        easing: 'ease', // 가속도
+    });
+
+
 
 })
